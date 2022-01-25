ba45f0aaa6a46c9c09f719c1f0703176cdba5454
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-01-25 12:34:23 +0100

    update

--- prvih 15/funkcije.js
+++ prvih 15/funkcije.js
@@ -10,6 +10,7 @@ function hrana(jelo1, jelo2, jelo3) {
 // console.log(hrana("musaku", "ćevape", "burek"));
 
 
+
 //Zadatak 15 //////////////////////////////////////////////////////////
 // 15. Napraviti novi dokument "funkcije.js". U dokumentu:
 // a) napisati funkciju koja će vršiti proračun zapremine valjka i vraćati je. Dati joj ime "zapremina". Funkciju napisati tako da može računati zapreminu bilo kojeg valjka, ako je poznat poluprečnik valjka i njegova visina. Ovi podaci se prosleđuju u vidu parametra funkcije.
